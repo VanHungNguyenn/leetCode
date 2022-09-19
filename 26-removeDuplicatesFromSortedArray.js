5fdbcf415c469a4907689437ec17b0e0bcf195f0
Van Hung <vanhungnguyenbku@gmail.com>
2022-09-20 00:03:56 +0700

    complete 26

---
+++ 26-removeDuplicatesFromSortedArray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const removeDuplicates = (nums) => {
+	let i = 0
+
+	for (let j = 1; j < nums.length; j++) {
+		if (nums[i] !== nums[j]) {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+
+	return i + 1
+}
+
+console.log(removeDuplicates([1, 1, 2]))
